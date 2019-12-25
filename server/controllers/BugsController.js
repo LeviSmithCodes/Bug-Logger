368eb9e2bc29b74bc131a340cdf7475777a9a8de
LeviSmithCodes <eyevel@gmail.com>
2019-12-25 09:50:42 -0700

    Got most of bug routes and logic up

--- server/controllers/BugsController.js
+++ server/controllers/BugsController.js
@@ -4,15 +4,45 @@ import bugService from "../services/bugService";
 export default class BugController {
   constructor() {
     this.router = express
-      .Router()
+      .Router({ mergeParams: true }) // allows getting params from main?
       //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
-      .get("", this.getAll);
+      .get("", this.getAll)
+      .get("/:id", this.getById)
+      .put("/:id", this.edit)
+      .post("", this.create);
   }
 
   async getAll(req, res, next) {
     try {
       let data = await bugService.getAll();
       return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      let data = await bugService.getById(req.params.id);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      let data = await bugService.edit(req.params.id, req.body);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      let data = await bugService.create(req.body);
+      return res.status(201).send(data);
     } catch (error) {
       next(error);
     }
--- server/services/BugService.js
+++ server/services/BugService.js
@@ -1,9 +1,37 @@
 import mongoose from "mongoose";
 import Bug from "../models/Bug";
+import ApiError from "../utils/ApiError";
 
 const _repository = mongoose.model("Bug", Bug);
 
 class BugService {
+  async edit(id, update) {
+    let data = await _repository.findById(id);
+
+    if (!data) {
+      throw new ApiError("Invalid ID", 400);
+    }
+
+    console.log(data);
+    if (data.closed == false) {
+      data = await _repository.findOneAndUpdate({ _id: id }, update); // NOTE what does the {_id: id} bit do? _id comes from the server, yes?
+    } else {
+      throw new ApiError("Cannot edit closed bug", 403);
+    }
+
+    return data;
+  }
+  async getById(id) {
+    let data = await _repository.findById(id);
+    if (!data) {
+      throw new ApiError("Invalid ID", 400);
+    }
+    return data;
+  }
+  async create(rawData) {
+    let data = await _repository.create(rawData);
+    return data;
+  }
   async getAll() {
     return await _repository.find({});
   }
