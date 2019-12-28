c8b0375be8ff12d169a66db40f9de308efb54e4a
LeviSmithCodes <eyevel@gmail.com>
2019-12-28 15:09:59 -0700

    Cleaned out hello world and about vues, got bugs to draw to the page

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,31 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+let _myServer = axios.create({
+  baseURL: "http://localhost:3000/api",
+  timeout: 6000
+});
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    bugs: [],
+    activeBug: {},
+    notes: []
   },
   mutations: {
+    setAllBugs(state, data) {
+      state.bugs = data;
+    }
   },
   actions: {
+    async getAllBugs({ commit, dispatch }) {
+      let res = await _myServer.get("bugs");
+      console.log(res.data);
+      commit("setAllBugs", res.data); // not data.data because we're not on the herokuapp sandbox
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,18 +2,27 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <h1>THIS IS A FRONT END LOL</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>{{ bugs }}</p>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  mounted() {
+    this.$store.dispatch("getAllBugs");
+  },
+  computed: {
+    bugs() {
+      return this.$store.state.bugs;
+    }
   }
+  // components: {
+  //   HelloWorld
+  // }
 };
 </script>
